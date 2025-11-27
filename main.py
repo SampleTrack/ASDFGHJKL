8b4b68aaaef8d6c19d24543c696081fdcc1c8683
SampleTrack <FirstSample@proton.me>
2025-11-28 00:18:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,68 @@
-from aiohttp import web
-from plugins import web_server
-from typing import Union, Optional, AsyncGenerator
 import asyncio
+import threading
+import logging
 from pyrogram import Client, idle
+from flask import Flask
 from config import Telegram, Server
 from helper.logger_setup import init_logger
 from helper.price_checker import run_price_check
 
-# Import the server starter
-from server import start_web_server
+
+web_app = Flask(__name__)
+
+
+@web_app.route("/")
+def hello_world():
+    return "Hello, World!"
+
 
 async def price_check_runner(client: Client):
     while True:
         await run_price_check(client, manual_trigger=False)
-        await asyncio.sleep(18000)
+        await asyncio.sleep(18000)  
 
-class Bot(Client):
 
-    def __init__(self):
-        super().__init__(
-            name=Telegram.BOT_NICKNAME,
-            api_id=Telegram.API_ID,
-            api_hash=Telegram.API_HASH,
-            bot_token=Telegram.BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
-        )
+def run_flask():
+    try:
+        log = logging.getLogger("werkzeug")
+        log.setLevel(logging.ERROR)
+        web_app.logger.setLevel(logging.ERROR)
+        web_app.run(host="0.0.0.0", port=7860)
+    except Exception:
+        logging.exception("Flask server crashed!")
 
-    async def start(self):
+
+app = Client(
+    Telegram.BOT_NICKNAME,
+    api_id=Telegram.API_ID,
+    api_hash=Telegram.API_HASH,
+    bot_token=Telegram.BOT_TOKEN,
+    plugins=dict(root="plugins"),
+)
+
+
+def main():
+    async def startup():
         try:
             await app.send_message(Telegram.ADMIN, "Bot restarted")
+            # start background task after bot is up
             asyncio.create_task(price_check_runner(app))
         except Exception as e:
             print(f"Failed to send message: {e}")
-            
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, "8080").start()
 
-    async def stop(self, *args):
-        await super().stop()
-        logging.info("Bot stopped. Bye.")
+    # initiate logger
+    logger = init_logger(app, __name__)
+    print("Bot started")
 
-app = Bot()
-app.run()
+    if Server.IS_SERVER:
+        flask_thread = threading.Thread(target=run_flask, daemon=True)
+        flask_thread.start()
+
+    app.start()
+    app.loop.run_until_complete(startup())
+    idle()
+    app.stop()
+
+
+if __name__ == "__main__":
+    main()
