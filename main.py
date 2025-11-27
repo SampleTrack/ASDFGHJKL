caef2533572fc33d624a2026e182dda321f81ded
SampleTrack <FirstSample@proton.me>
2025-11-27 23:55:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class Bot(Client):
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+        await web.TCPSite(app, bind_address, "8080").start()
 
 
 app = Bot()
