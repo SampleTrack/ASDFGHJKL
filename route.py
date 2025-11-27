0aeab22686a6eea67c68f6057655f8212a496130
SampleTrack <FirstSample@proton.me>
2025-11-28 00:16:07 +0530

    Delete route.py

--- route.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-# Create a Blueprint (a group of routes)
-web_routes = Blueprint('web_routes', __name__)
-
-@web_routes.route("/")
-def hello_world():
-    return "Hello, World! Bot is running."
