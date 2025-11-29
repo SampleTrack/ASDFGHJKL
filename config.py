899bab7b52b1b0236fc74190154082ca03088ad7
SampleTrack <FirstSample@proton.me>
2025-11-29 17:14:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,4 +35,4 @@ DATABASE_NAME = os.getenv('DATABASE_NAME', "")
 
 # === SERVER SETTINGS === #
 PORT = int(os.getenv("PORT", 8080))
-IS_SERVER = os.getenv("IS_SERVER", "False").lower() == "true" 
+IS_SERVER = os.getenv("IS_SERVER", "TRUE").lower() == "TRUE" 
