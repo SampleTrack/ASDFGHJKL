a17c195ba8f52de87c1337fe13bd73ff69b36ea3
SampleTrack <FirstSample@proton.me>
2025-11-29 13:44:18 +0530

    Update report_error.py

--- helper/report_error.py
+++ helper/report_error.py
@@ -1,14 +1,14 @@
 
 #code to save in utils
 from pyrogram import enums
-from config import Telegram
+from config import ERROR_LOGGER_ID
 
 async def report_error(app, error_message: str):
     try:
         await app.send_message(
-            chat_id=Telegram.ERROR_LOGGER_ID,
+            chat_id=ERROR_LOGGER_ID,
             text=f"**#Error :**\n{error_message}",
-            reply_to_message_id=Telegram.ERROR_LOGGER_TOPIC,
+            reply_to_message_id=ERROR_LOGGER_TOPIC,
             parse_mode=enums.ParseMode.MARKDOWN
         )
     except Exception as e:
