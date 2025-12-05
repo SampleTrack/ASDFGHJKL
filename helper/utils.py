ea5b38e6c76806ea0b197ac714c5c34c79b2ba4b
SampleTrack <FirstSample@proton.me>
2025-12-05 18:03:47 +0530

    Create utils.py

---
+++ helper/utils.py
@@ -0,0 +1,30 @@
+import aiohttp
+import logging
+from config import Config
+
+logger = logging.getLogger(__name__)
+
+async def fetch_product_info(url):
+    """
+    Fetches product details from the API.
+    """
+    api_url = "https://e-com-price-tracker-lemon.vercel.app/buyhatke"
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(api_url, params={"product_url": url}, timeout=20) as resp:
+                if resp.status != 200:
+                    return None
+                data = await resp.json()
+                
+                if "error" in data or "detail" in data:
+                    return None
+                
+                return data
+        except Exception as e:
+            logger.error(f"API Fetch Error: {e}")
+            return None
+
+def get_readable_time(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return f"{int(h):02d}:{int(m):02d}:{int(s):02d}"
